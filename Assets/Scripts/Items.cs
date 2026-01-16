94589f646865fcf9601f64c22e5040e339d248a2
TheBlaze <santi.volcanos@gmail.com>
2026-01-16 10:31:47 -0600

    items script rework

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="NewItem",menuName = "Items/Item")]
 public class Items :ScriptableObject
 {
-   [SerializeField]ItemCategories category = ItemCategories.none;
-   [SerializeField] int monetaryValue;
-   [SerializeField] int emotionalValue;
-    [SerializeField] Sprite itemSprite;
+   public ItemCategories category = ItemCategories.none;
+    public int monetaryValue;
+    public int emotionalValue;
+     public Sprite itemSprite;
 }
 
 
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -9,6 +9,8 @@ public class ScoreManager : MonoBehaviour
 
     int debt = 372000;
     int emotionalSanity = 4;
+    ItemCategories playerLike1;
+    ItemCategories playerLike2;
     
 
     private void Awake()
@@ -24,21 +26,33 @@ public class ScoreManager : MonoBehaviour
             }
         }
         UpdateDebt();
+
+        int like1=Random.Range(0, 6);
+        int like2=Random.Range(0, 6);
+        while (like2 == like1)
+        {
+            like2=Random.Range(0, 6);
+        }
+        playerLike1 = (ItemCategories)like1;
+        playerLike2 = (ItemCategories)like2;
     }
     void UpdateDebt()
     {
         onScreenDebt.text = debt.ToString();
     }
+
+    
+
 }
 
 
 public enum ItemCategories
 {
-    Music,
-    Food,
-    Drinks,
-    Alcohol,
-    Style,
-    Art,
-    none
+    Music=0,
+    Food=1,
+    Drinks=2,
+    Alcohol=3,
+    Style=4,
+    Art=5,
+    none=6
 }
