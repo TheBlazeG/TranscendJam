15ae1eeec003b4e3089bd7efa580fceb9efbea11
TheBlaze <santi.volcanos@gmail.com>
2026-01-16 13:04:24 -0600

    untested Sell, keep, debt mechanics and random selection of items added

--- Assets/Scripts/DraggableItems.cs
+++ Assets/Scripts/DraggableItems.cs
@@ -9,27 +9,33 @@ public class DraggableItems : MonoBehaviour
     public float sensitivity;
     [SerializeField] GameObject storageArea;
     [SerializeField] LayerMask areasLayer;
+    public List<Items> itemsToDecide;
     Collider2D results;
     private void Start()
     {
+        //obtener componente de spriteRenderer
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnMouseDrag()
     {
-        spriteRenderer.color = Color.black;
+        //cambia el color del sprite mas grisáceo para dar feedback y mueve el objeto según el mouse delta mientras hace el mouse invisible
+        spriteRenderer.color = Color.lightGray;
         transform.position += new Vector3(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"),0)*Time.deltaTime*sensitivity;
         Cursor.visible = false;
     }
 
     private void OnMouseUp()
     {
+        //revisa si soltaste el objeto en alguna zona de guardado, sea venta o guardar
         results = Physics2D.OverlapCircle(transform.position, 2, areasLayer);
         if (results!=null)
         {
             storageArea = results.gameObject;
         }
 
+
+        //regresa el color del objeto al color original, y teletransporta el mouse al objeto antes de hacerlo visible
         spriteRenderer.color= Color.white;
         var v = Camera.main.WorldToScreenPoint(transform.position);
         Mouse.current.WarpCursorPosition(v);
@@ -38,17 +44,26 @@ public class DraggableItems : MonoBehaviour
         {
             if (storageArea.CompareTag("Keep"))
             {
-                //codigo de preservar objeto
+                //código para llamar la funcion de keep
+                Debug.Log("kept");
+                ScoreManager.instance.KeepItem();
             }
             else if (storageArea.CompareTag("Sell"))
             {
-                //codigo de vender objeto
+                //código para llamar la función de sell
+                Debug.Log("sold");
+                ScoreManager.instance.SellItem();
             }
-            gameObject.SetActive(false);
+            storageArea = null;
+            transform.position = Vector2.zero;
+
         }
     }
 
-
+    public void UpdateSprite(int index)
+    {
+        spriteRenderer.sprite= itemsToDecide[index].itemSprite;
+    }
 
     //private void OnTriggerEnter(Collider other)
     //{
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,13 +1,20 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
 
     [SerializeField] TextMeshProUGUI onScreenDebt;
-
+    [SerializeField] TextMeshProUGUI onScreenItemCategory;
+    [SerializeField] public Image[] onScreenSanity;
+    Items currentItem;
+    DraggableItems items;
+    int currentItemIndex=0;
+    int totalValueOfItems;
     int debt = 372000;
+    public int interestPerSecond = 0;
     int emotionalSanity = 4;
     ItemCategories playerLike1;
     ItemCategories playerLike2;
@@ -15,6 +22,7 @@ public class ScoreManager : MonoBehaviour
 
     private void Awake()
     {
+        //singleton
         if (instance == null)
         {
             instance = this;
@@ -25,10 +33,16 @@ public class ScoreManager : MonoBehaviour
                 Destroy(this);
             }
         }
+        currentItem = items.itemsToDecide[currentItemIndex];
+        items.UpdateSprite(currentItemIndex);
+        UpdateItemCategory();
         UpdateDebt();
+        UpdateEmotionalSanity();
+        UpdateValue();
 
-        int like1=Random.Range(0, 6);
-        int like2=Random.Range(0, 6);
+        //gustos del personaje asignados de manera aleatoria
+        int like1=Random.Range(0, 7);
+        int like2=Random.Range(0, 7);
         while (like2 == like1)
         {
             like2=Random.Range(0, 6);
@@ -36,16 +50,116 @@ public class ScoreManager : MonoBehaviour
         playerLike1 = (ItemCategories)like1;
         playerLike2 = (ItemCategories)like2;
     }
+
+    private void Update()
+    {
+        AddDebt();
+        if (debt>totalValueOfItems)
+        {
+            
+        }
+    }
+
+    private void UpdateItemCategory()
+    {
+        onScreenItemCategory.text=currentItem.category.ToString();
+    }
+
+    //función para actualizar el valor emocional visible en UI
+    private void UpdateEmotionalSanity()
+    {
+        for (int i = 0; i < onScreenSanity.Length; i++)
+        {
+            if (i < emotionalSanity)
+            {
+            onScreenSanity[i].gameObject.SetActive(true);
+            }
+            else
+            {
+            onScreenSanity[i].gameObject.SetActive(false);
+            }
+        }
+    }
+    //función para actualizar el valor monetario disponible visible en UI
+    private void UpdateValue()
+    {
+        totalValueOfItems-=currentItem.monetaryValue;
+        Debug.Log("no has agregado el codigo para objetos");
+    }
+    //función para actualizar el monto de deuda visible en UI
     void UpdateDebt()
     {
         onScreenDebt.text = debt.ToString();
     }
 
+    //función para vender item; drena valor emocional a la sanidad, resta monto disponible y resta dinero a la deuda
+    public void SellItem()
+    {
+        int emotionalBonus;
+        if (currentItem.category==playerLike1||currentItem.category == playerLike2)
+        {
+             emotionalBonus = 1;
+        }
+        else
+        {
+             emotionalBonus = 0;
+        }
+        emotionalSanity -=(currentItem.emotionalValue+emotionalBonus);
+        debt-=currentItem.monetaryValue;
+        UpdateDebt();
+        UpdateValue();
+        UpdateEmotionalSanity();
+        if (items.itemsToDecide.Count>0)
+        {
+            items.itemsToDecide.Remove(currentItem);
+            currentItemIndex = Random.Range(0,items.itemsToDecide.Count);
+        currentItem=items.itemsToDecide[currentItemIndex];
+            UpdateItemCategory();
+            items.UpdateSprite(currentItemIndex);
+        }
+        else
+        {
+            //lose code
+        }
+    }
+
+    //función para guardar item; drena monto disponible y suma sanidad
+    public void KeepItem()
+    {
+        int emotionalBonus;
+        if (currentItem.category == playerLike1 || currentItem.category == playerLike2)
+        {
+            emotionalBonus = 1;
+        }
+        else
+        {
+            emotionalBonus = 0;
+        }
+        emotionalSanity +=(currentItem.emotionalValue+emotionalBonus);
+        UpdateValue();
+        UpdateEmotionalSanity();
+        if (items.itemsToDecide.Count > 0)
+        {
+            items.itemsToDecide.Remove(currentItem);
+            currentItemIndex = Random.Range(0, items.itemsToDecide.Count);
+            currentItem = items.itemsToDecide[currentItemIndex];
+            UpdateItemCategory();
+            items.UpdateSprite(currentItemIndex);
+        }
+        else 
+        {
+        //lose code
+        }
+    }
     
+    public void AddDebt()
+    {
+        debt += interestPerSecond;
+    }
 
 }
 
-
+//enum para categorizar los items y designar gustos del jugador
 public enum ItemCategories
 {
     Music=0,
@@ -54,5 +168,6 @@ public enum ItemCategories
     Alcohol=3,
     Style=4,
     Art=5,
-    none=6
+    Games=6,
+    none=7
 }
